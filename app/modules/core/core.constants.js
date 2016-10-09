5133b99617b682f30640fdbf6c223dbdd9cde3e2
ecaminero <caminero.edwin@gmail.com>
2016-10-09 12:39:54 -0300

    Cambio de enviroment

--- app/modules/core/core.constants.js
+++ app/modules/core/core.constants.js
@@ -3,7 +3,8 @@
 
     angular
         .module("app.core")
-        .constant("BASE_API_URI", "http://127.0.0.1:5000")
+        //.constant("BASE_API_URI", "http://127.0.0.1:5000")
+        .constant("BASE_API_URI", "https://agenceevaluation-ecaminero.c9users.io")
         .constant("moment", moment)
         .constant("_", window._);
 })();
