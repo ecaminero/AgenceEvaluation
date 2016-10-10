5957c2ddb52c094439a6a3879c471d42c2c5373a
ecaminero <caminero.edwin@gmail.com>
2016-10-10 14:38:12 -0300

    show ip request

--- api/controllers/consultant.js
+++ api/controllers/consultant.js
@@ -9,36 +9,40 @@ module.exports = { getAll, getAverageFixedCost, getEarning, getPerformanceComerc
 
 //GET /consultant 
 function getAll(req, res, next) {
-	db.find().then(function (values) {
-		res.json({ data: values });
-	});
+  console.log("INF", req.ip);
+  db.find().then(function (values) {
+      res.json({ data: values });
+  });
 }
 //POST /consultant/average 
 function getAverageFixedCost(req, res, next) {
-		var consultants = req.body.consultants;
-		db.averageFixedCost(consultants).then(function (response) {
-				res.json({ data: response });
+  console.log("INF", req.ip);
+    var consultants = req.body.consultants;
+    db.averageFixedCost(consultants).then(function (response) {
+            res.json({ data: response });
 
-		});
+    });
 }
 
 //POST /consultant/earnings 
 function getEarning(req, res, next) {
-	var consultants = req.body.consultants;
-	var monthstart = req.body.monthStart;
-	var monthend = req.body.monthEnd;
-	db.earnings(consultants, monthstart, monthend).then(function (response) {
-		res.json({ data: response });
-	});
+  console.log("INF", req.ip);
+  var consultants = req.body.consultants;
+  var monthstart = req.body.monthStart;
+  var monthend = req.body.monthEnd;
+  db.earnings(consultants, monthstart, monthend).then(function (response) {
+      res.json({ data: response });
+  });
 
 }
 
 //POST /consultant/performance 
 function getPerformanceComercial(req, res, next) {
-		var consultants = req.body.consultants;
-		var monthstart = req.body.monthStart;
-		var monthend = req.body.monthEnd;
-		db.performanceComercial(consultants, monthstart, monthend).then(function (response) {
-			res.json({ data: response });
-		});
+  console.log("INF", req.ip);
+  var consultants = req.body.consultants;
+  var monthstart = req.body.monthStart;
+  var monthend = req.body.monthEnd;
+  db.performanceComercial(consultants, monthstart, monthend).then(function (response) {
+      res.json({ data: response });
+  });
 }
